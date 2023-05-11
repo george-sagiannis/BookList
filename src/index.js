eccced2372602a77beb64fc2b0cef862bae60373
Σαγιάννης Γεώργιος <G.Sagiannis@deddie.gr>
2023-05-11 10:19:47 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import ReactDOM from "react-dom/client"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import "./index.css"
+
+const books = [
+  {
+    author: "Maximilian Schwarzmuller",
+    title: "React Key Concepts: Consolidate your knowledge of React's core features",
+    img: "https://m.media-amazon.com/images/I/41rgivhpOaL._SX404_BO1,204,203,200_.jpg",
+  },
+  {
+    author: "Robin Wieruch",
+    title: "The Road to React: The React.js with Hooks in JavaScript Book (2023 Edition)",
+    img: "https://m.media-amazon.com/images/I/61N1Yn4Nf5L._AC_UY327_FMwebp_QL65_.jpg",
+  },
+]
+
+const names = ["john", "peter", "susan"]
+
+const newNames = names.map((name) => {
+  console.log(name)
+  return <h1>{name}</h1>
+})
+
+console.log(newNames)
+
+function BookList() {
+  return <section className="booklist">{newNames}</section>
+}
+
+const Book = (props) => {
+  const { img, title, author } = props
+  console.log(props)
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author} </h4>
+    </article>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<BookList />)

c213d77694f92c84bb9de53fc2e803dd78627e11
g.sagiannis <g.sagiannis@deddie.gr>
2023-05-30 13:51:01 +0300

    i take the title of each book in console when i click the button

--- src/index.js
+++ src/index.js
@@ -23,55 +23,31 @@ const books = [
 function BookList() {
   return (
     <section className='booklist'>
-      <EventExamples />
       {books.map((book) => {
-        console.log(book);
-        const { img, title, author, id } = book;
-            return <Book {...book} key={book.id} />;
+        return <Book {...book} key={book.id} />;
       })}
     </section>
   );
 }
 
-//perasa ta events
-//an grapso mesa sto input tha fanei sto console
-//an patiso to click me tha mou ferei to console
-const EventExamples = () => {
+const Book = (props) => {
+  const { img, title, author } = props;
+  const displayTitle = () => {
+    console.log(title); //it triggers the displayTitle function. The displayTitle function logs the title variable to the console using console.log(title).
+  };
+
   return (
-    <section>
-      <form>
-        <h2>Typical Form</h2>
-        <input
-          type='text'
-          name='example'
-          onChange={(e) => console.log(e.target.value)}
-          style={{ margin: '1rem 0' }}
-        />
-      </form>
-      <button onClick={() => console.log('you clicked me')}>click me</button>
-    </section>
+    <article className='book'>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <button onClick={displayTitle}>Δείξε μου τον τίτλο στην κονσόλα</button>
+      <h4>{author} </h4>
+    </article>
   );
 };
 
-// The input field has an onChange event handler that logs the value entered into the input field whenever it changes. The button has an onClick event handler that logs a message when clicked.
+// In the given code, when i click the "Δείξε μου τον τίτλο στην κονσόλα" button, it triggers the displayTitle function. The displayTitle function logs the title variable to the console using console.log(title).
 
-// The (e) parameter in the onChange event handler function is a convention to represent the event object. In this case, it represents the synthetic event object for the onChange event. You can access properties of the event object, such as target.value, to get the current value of the input field.
-
-
-
-
-
-const Book = (props) => {
-  const { img, title, author } = props
-  console.log(props)
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author} </h4>
-    </article>
-  )
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 

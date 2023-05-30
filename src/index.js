64ba1bd09d04e2a5c11c14ed46c3470cc6999f1a
g.sagiannis <g.sagiannis@deddie.gr>
2023-05-30 14:14:12 +0300

    example

--- src/index.js
+++ src/index.js
@@ -20,31 +20,44 @@ const books = [
 ]
 
 
-function BookList() {
+// accepts id as an argument and finds the book
+// pass the function down to Book Component and invoke on the button click
+
+
+const BookList = () => {
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book);
+  };
+
   return (
     <section className='booklist'>
       {books.map((book) => {
-        return <Book {...book} key={book.id} />;
+        return <Book {...book} key={book.id} getBook={getBook} />;
       })}
     </section>
   );
-}
+};
 
 const Book = (props) => {
-  const { img, title, author } = props;
-  const displayTitle = () => {
-    console.log(title); //it triggers the displayTitle function. The displayTitle function logs the title variable to the console using console.log(title).
+  const { img, title, author, getBook, id } = props;
+  // console.log(props);
+  const getSingleBook = () => {
+    getBook(id);
   };
-
   return (
     <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <button onClick={displayTitle}>Δείξε μου τον τίτλο στην κονσόλα</button>
-      <h4>{author} </h4>
+
+      <button onClick={() => getBook(id)}>ΔΕΙΞΕ ΜΟΥ ΤΟΝ ΤΙΤΛΟ</button>
+      <h4>{author}</h4>
     </article>
   );
 };
+
+
+
 
 // In the given code, when i click the "Δείξε μου τον τίτλο στην κονσόλα" button, it triggers the displayTitle function. The displayTitle function logs the title variable to the console using console.log(title).
 
